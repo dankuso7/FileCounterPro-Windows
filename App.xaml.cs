584a45282db895ac1a1b96a889c2e6481ab1f6b4
AnkanRoy <chandanaroy2222@gmail.com>
2026-07-04 20:30:43 +0530

    🐛 Fix VM crash by forcing Software Rendering

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,16 @@
 using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace FileCounterPro_Windows
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Force Software Rendering for Virtual Machines (UTM/Parallels) to prevent DirectX crashes
+            RenderOptions.ProcessRenderMode = RenderMode.SoftwareOnly;
+            base.OnStartup(e);
+        }
     }
 }
